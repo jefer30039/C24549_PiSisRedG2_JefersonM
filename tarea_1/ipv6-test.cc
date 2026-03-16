d3fadf542336d7324d6ea4b70f2ad5946c961fb0
jefer30039 <jefer30039@gmail.com>
2026-03-15 21:36:33 -0600

    ipv6

---
+++ tarea_1/ipv6-test.cc
@@ -0,0 +1,36 @@
+/**
+  *  Universidad de Costa Rica
+  *  ECCI
+  *  CI0123 Proyecto integrador de redes y sistemas operativos
+  *  2026-i
+  *  Grupos: 2 y 3
+  *
+  *
+  *  Esta prueba solo funciona utilizando un equipo de la red interna de la ECCI, por lo que
+  *  deberan realizarlo en la ECCI o  conectarse por la VPN para completarla
+  *  La direccion IPv6 provista es una direccion privada
+  *  Tambien deben prestar atencion al componente que esta luego del "%" en la direccion y que hace
+  *  referencia a la interfaz de red utilizada para la conectividad, en el ejemplo se presenta la interfaz "eno1"
+  *  pero es posible que su equipo tenga otra interfaz
+  *
+ **/
+
+#include <stdio.h>
+#include <string.h>
+#include "Socket.h"
+
+int main( int argc, char * argv[] ) {
+   const char * lab = "::1";
+   const char * request = "GET /ip HTTP/1.1\r\nHost: localhost\r\n\r\n";
+
+   Socket s( 's', true );
+   char a[512];
+
+   memset( a, 0, 512 );
+   s.Connect( lab, 8080 );
+   s.Write( request );
+   s.Read( a, 512 );
+   printf( "%s\n", a);
+
+}
+
